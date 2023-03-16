dbab78c55586cb861a06823132753aebe9aaaa51
irem <iremaydinn.7@gmail.com>
2023-03-16 23:42:29 +0300

    player jump movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed = 15f;
     [SerializeField] private float jumpHeight = 1.5f;
     private float currentSpeed = 0f;
+    
     // rotation control
-
     [SerializeField] private float speedSmoothVelocity = 0f;
     [SerializeField] private float speedSmoothTime = 0.1f;
     [SerializeField] private float rotationSpeed = 0.01f;
-    private float gravity = 3f;
-
-    //camera
-    private Transform mainCameraTransform = null;
     
-    // controllers
+    
+    //jumping parameters and ground check
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    public Transform groundCheck;
+    public float gravity = -9.81f;
+
+    private float targetSpeed = 0f;
+    public Vector3 gravityVector;
+    
+    // camera and controllers
+    private Transform mainCameraTransform = null;
     private CharacterController charController = null;
     private Animator anim = null;
 
@@ -40,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
         // get input from player
         Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
@@ -52,61 +60,66 @@ public class PlayerMovement : MonoBehaviour
         right.y = 0;
         forward.Normalize();
         right.Normalize();
-
         
         Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
-
-        Vector3 gravityVector = Vector3.zero;
-       
-        // if character is not grounded he should fall
-        if (!charController.isGrounded)
-        {
-            gravityVector.y -=gravity;
-        }
         
+        // if character is not grounded he should fall
+        if (isGrounded && gravityVector.y < 0)
+        {
+            gravityVector.y -= 2f;
+        }
+
 
         if (desiredMoveDirection != Vector3.zero)
         {
             //spherically interpolates between a and b
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection),
+                rotationSpeed);
         }
-        
+
         // the speed that you use as threshold
-        float targetSpeed = walkSpeed * movementInput.magnitude;
-        
-        
+        targetSpeed = walkSpeed * movementInput.magnitude;
+
+
         // if left shift is pressed player should run
         if (Input.GetKey(KeyCode.LeftShift))
         {
             targetSpeed = runSpeed * movementInput.magnitude;
-            
+
         }
-        
 
         // walk animation
-        anim.SetFloat("Speed", 0.5f * movementInput.magnitude, speedSmoothTime,Time.deltaTime );
-        
+        anim.SetFloat("Speed", 0.5f * movementInput.magnitude, speedSmoothTime, Time.deltaTime);
+
         // run animation 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            anim.SetFloat("Speed", 1f * movementInput.magnitude, speedSmoothTime,Time.deltaTime );
+            anim.SetFloat("Speed", 1f * movementInput.magnitude, speedSmoothTime, Time.deltaTime);
         }
-        
+
         // pet the animal
         if (Input.GetKey(KeyCode.E) && desiredMoveDirection == Vector3.zero)
         {
-            
+
             StartCoroutine(PetAnimal());
         }
-        
+
         // add throw and jump animations
-        gravityVector.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
- 
-        // to adjust speed changes
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            gravityVector.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
         
+        // to adjust speed changes
+        
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
         charController.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
+        gravityVector.y += gravity * Time.deltaTime;
         charController.Move(gravityVector * Time.deltaTime);
+
+
+            
     }
 
     public IEnumerator PetAnimal()
